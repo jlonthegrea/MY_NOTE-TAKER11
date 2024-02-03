2f57d34d924e3e5f529a043ca1d5417c4944bd99
jlonthegrea <allen_jaylon@yahoo.com>
2024-02-03 15:28:37 -0500

    Ability to add notes and post them completed

---
+++ Develop/routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const {
+    notes
+} = require('../db/db');
+const {
+    createNote
+} = require('../../library/noteFunctions');
+
+router.get('/notes', (req, res) => {
+    let saved = notes;
+    res.json(saved);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = createNote(req.body, notes);
+    res.json(note);
+})
+
+module.exports = router;
